5d6f5092239d69ae1c7de82b25a9f15b7d606783
Gerard Gomez <gerardalexigomez@yahoo.com>
2021-08-22 18:02:03 +1200

    basic gameplay added

--- game/Game.js
+++ game/Game.js
@@ -1,26 +1,93 @@
 /** @type {import("../typing/phaser")} */
 class Game extends Phaser.Scene {
-    constructor() {
-        super({key: 'Game'});
+  constructor() {
+    super({ key: "Game" });
+  }
+
+  preload() {
+    this.load.image("cat", "assests/images/player-spriteS.png");
+    this.load.image("pipe", "assests/images/default-pipe-sprite.png");
+  }
+
+  create() {
+    this.cat = this.physics.add.sprite(80, game.config.height / 2, "cat");
+    this.cat.body.gravity.y = gameOptions.catGravity;
+    this.input.on("pointerdown", this.flap, this);
+    this.pipeGroup = this.physics.add.group();
+    this.pipePool = [];
+    for (let i = 0; i < 4; i++) {
+      this.pipePool.push(this.pipeGroup.create(0, 0, "pipe"));
+      this.pipePool.push(this.pipeGroup.create(0, 0, "pipe"));
+      this.placePipes();
     }
+    this.pipeGroup.setVelocityX(-gameOptions.catSpeed);
+  }
 
-    preload() {
-
+  update() {
+    this.physics.world.collide(
+      this.cat,
+      this.pipeGroup,
+      function () {
+        this.die();
+      },
+      null,
+      this
+    );
+    if (this.cat.y > game.config.height || this.cat.y < 0) {
+      this.die();
     }
+    this.pipeGroup.getChildren().forEach(function (pipe) {
+      if (pipe.getBounds().right < 0) {
+        this.pipePool.push(pipe);
+        if (this.pipePool.length == 2) {
+          this.placePipes(true);
+        }
+      }
+    }, this);
+  }
 
-    create() {
+  getEvents() {
+    // create all event obj
+  }
 
-    }
-
-    update() {
-        
-    }
-
-    getEvents() {
-        // create all event obj
-    }
-
-    pickEvent() {
-        // pick a random event from event objs
-    }
+  pickEvent() {
+    // pick a random event from event objs
+  }
+  flap() {
+    this.cat.body.velocity.y = -gameOptions.catFlapPower;
+  }
+  placePipes() {
+    let rightmost = this.getRightMostPipe();
+    let pipeHoleHeight = Phaser.Math.Between(
+      gameOptions.pipeHole[0],
+      gameOptions.pipeHole[1]
+    );
+    let pipeHolePosition = Phaser.Math.Between(
+      gameOptions.minPipeHeight + pipeHoleHeight / 2,
+      game.config.height - gameOptions.minPipeHeight - pipeHoleHeight / 2
+    );
+    this.pipePool[0].x =
+      rightmost +
+      this.pipePool[0].getBounds().width +
+      Phaser.Math.Between(
+        gameOptions.pipeDistance[0],
+        gameOptions.pipeDistance[1]
+      );
+    this.pipePool[0].y = pipeHolePosition - pipeHoleHeight / 2;
+    this.pipePool[0].setOrigin(0, 1);
+    this.pipePool[1].x = this.pipePool[0].x;
+    this.pipePool[1].y = pipeHolePosition + pipeHoleHeight / 2;
+    this.pipePool[1].setOrigin(0, 0);
+    this.pipePool = [];
+  }
+  getRightMostPipe() {
+    let rightmostPipe = 0;
+    this.pipeGroup.getChildren().forEach(function (pipe) {
+      rightmostPipe = Math.max(rightmostPipe, pipe.x);
+    });
+    return rightmostPipe;
+  }
+  die() {
+    this.scene.start("Game");
+  }
 }
--- game/gameConfig.js
+++ game/gameConfig.js
@@ -1,12 +1,28 @@
 /** @type {import("../typing/phaser")} */
 
 const config = {
-    type: Phaser.AUTO,
-    width: 600,
-    height: 800,
-    backgroundColor: '#6C3FDD',
-    scene: [MainMenu, Game, ShopMenu],
+  type: Phaser.AUTO,
+  width: 600,
+  height: 800,
+  backgroundColor: "#6C3FDD",
+  scene: [Game],
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 0,
+      },
+    },
+  },
+};
+const gameOptions = {
+  catSpeed: 125,
+  catGravity: 800,
+  catFlapPower: 300,
+  minPipeHeight: 50,
+  pipeDistance: [220, 280],
+  pipeHole: [100, 130],
 };
 
 const game = new Phaser.Game(config);
-
